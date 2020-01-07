46b228ad8d20dae7c3c6b2910b27317b6c4a762e
Asif Kamal <asif_kamal@hotmail.com>
2020-01-07 18:44:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,3 +17,23 @@ class Dog
   end
   
 end
+
+class Person
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def job=(persons_job)
+    @job = persons_job
+  end
+  
+  def job
+    @job
+  end
+  
+end
